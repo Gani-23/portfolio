3c7d66da7043c80e96589d487f188f6edd5f5459
Gani-23 <saiganesh7989@gmail.com>
2023-10-01 12:28:19 +0530

    Updates

---
+++ precache-manifest.0605c5e0e127ad5e4fc38a6d0f141750.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccddebf2d0a3cd5cbaf0",
+    "url": "/portfolio/static/css/main.c56377f6.chunk.css"
+  },
+  {
+    "revision": "ccddebf2d0a3cd5cbaf0",
+    "url": "/portfolio/static/js/main.ccddebf2.chunk.js"
+  },
+  {
+    "revision": "8bcc787db593634d27ed",
+    "url": "/portfolio/static/js/1.8bcc787d.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "edd0e116caf402a0880df775d2c6bac1",
+    "url": "/portfolio/index.html"
+  }
+];
